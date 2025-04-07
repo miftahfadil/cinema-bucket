b009ba307ad63473395165099c32bc04d380a9a0
Miftah Fadil <miftahfadilv@gmail.com>
2025-04-07 16:00:44 +0700

    starting login page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+const LoginPage = () => {
+    return (
+        <div>
+            <h1 className="text-blue-500">Login Page</h1>
+            <form action="">
+                <input type="text" name="" id="" className="border"/>
+                <input type="password" name="" id="" className="border"/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default LoginPage;
